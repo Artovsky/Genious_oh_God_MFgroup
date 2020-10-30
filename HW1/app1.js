dba5ffd0c642bb31050310bc078a83829d1d6a36
tayafedorova0 <72029222+tayafedorova0@users.noreply.github.com>
2020-10-30 20:12:45 +0300

    Add files via upload

---
+++ HW1/app1.js
@@ -0,0 +1,19 @@
+// let mas = [4, 2, 5, 1, 3, 8, 9, 6];
+// mas.sort(function(a, b){
+//   return b - a;
+// });
+// console.log(mas); 
+
+
+
+// let mas = [1, 2, 3, 4, 5, 6, 7, 8];
+// mas.splice(1, 1);
+// console.log(mas);
+// mas.splice(0, 3, 'name', 'age');
+// console.log(mas);
+// mas.splice(2, 0, 'city', 'adress');
+// console.log(mas);
+// let removed = mas.splice(0, 2);
+// console.log(mas);
+// mas.splice(2,4);
+// console.log(mas);
