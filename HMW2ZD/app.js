896e1f8e965e39df448836f78a665b83f0abf421
Hydradominatus <62065673+Hydradominatus@users.noreply.github.com>
2020-10-30 21:52:05 +0300

    Домашка

---
+++ HMW2ZD/app.js
@@ -0,0 +1,66 @@
+// to_all1
+
+// let arrsrt = [1,12,5,36,17]
+
+// arrsrt.sort(function (a, b) {
+//     if (a > b){
+//       return 1;
+//     }
+//     if (a < b) {
+//       return -1;
+//     }
+//     return 0;
+//     }
+// );
+
+// console.log(arrsrt)
+
+// to_all2
+
+// let prim1 = ['jija', 'jijaj', 'jijaji', 'jijajij', 'jijajija'];
+
+// let rem1 = prim1.splice(3, 2);
+
+// console.log(prim1)
+
+// console.log(rem1)
+
+// let prim2 = ['jija', 'jijaj', 'jijaji', 'jijajij', 'jijajija'];
+
+// let rem2 = prim2.splice(2, 2, 'nejija');
+
+// console.log(prim2)
+
+// console.log(rem2)
+
+// let prim3 = ['jija', 'jijaj'];
+
+// let rem3 = prim3.splice(0, 0, 'jijaji', 'jijajij', 'jijajija');
+
+// console.log(prim3)
+
+// console.log(rem3)
+
+// let prim4 = ['jija', 'jijaj', 'jijaji', 'jijajij', 'jijajija'];
+
+// let rem4 = prim4.splice(-2, 2);
+
+// console.log(prim4)
+
+// console.log(rem4)
+
+// let prim5 = ['jija', 'jijaj', 'jijaji', 'jijajij', 'jijajija'];
+
+// let rem5 = prim5.splice(0);
+
+// console.log(prim5)
+
+// console.log(rem5)
+
+// to_all3
+
+// let loyalists = ['Dark Angels', 'Thousand Sons', 'Blood Angels', 'Iron Hands', 'Imperial Fists'];
+
+// let heretics = loyalists.slice(1);
+
+// console.log(loyalists)
